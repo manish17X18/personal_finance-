fe6c6be1130e90b6fdc81898ac3961bdefbb056b
manish17x18 <manishdesh1710@gmail.com>
2026-04-24 17:15:49 +0530

    Added the createAsyncThunk

---
+++ src/slices/Rate.jsx
@@ -0,0 +1,37 @@
+import {createAsyncThunk,createSlice} from '@reduxjs/toolkit'
+
+const fetchRate=createAsyncThunk('transcations/fetchrate',async()=>{
+    const response=await fetch(`https://api.frankfurter.dev/v1/latest?base=USD`)
+    const data=await response.json();
+    return data.rates;
+})
+
+const TranscationsSlice=createSlice({
+    name:'rate',
+    initialState:{
+        transcations:[],
+        rates:{},
+        status:'idle'
+    },
+    reducers:{
+        add:(state,action)=>{
+            state.transcations.push(action.payload)
+        }
+    },
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchRate.pending,(state)=>{
+            state.status='load'
+        })
+        .addCase(fetchRate.fullfilled,(state,action)=>{
+            state.status='success';
+            state.rates=action.payload
+        })
+        .addCase(fetchRate.rejected,(state)=>{
+            state.status='fail'
+        })
+    }
+})
+
+export const {add}=TranscationsSlice.actions
+export default TranscationsSlice.reducer
