3c02c4330f6657b9f7d224d559c9262e093080b6
manish17x18 <manishdesh1710@gmail.com>
2026-04-26 23:20:59 +0530

    Updated

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import Transcations from '../slices/TransactionSlice'
 import RateExchange from '../slices/Rate'
+import SignUp from '../slices/SignUpSlice'
 export const store=configureStore({
     reducer:{
         transactions:Transcations,
-        rate:RateExchange
+        rate:RateExchange,
+        signup:SignUp
     }
 })
 
